660bc1b1b43289b2fe4727f082b0c7e19e39936b
Rodrigo Valin <licorna@gmail.com>
2019-09-17 15:23:43 +0100

    Removing MongoDB specifics.

--- kubeobject/deployment.py
+++ kubeobject/deployment.py
@@ -6,59 +6,23 @@ from kubernetes import client
 
 
 class Deployment:
+    """This is a super simple Deployment wrapper. The only reason it is here it is because I will use it
+    to deploy the MongoDB Kubernetes Operator.
+    """
     @classmethod
-    def create(cls, name, namespace, service_account: ServiceAccount):
+    def create(cls, name, namespace, service_account: ServiceAccount, container_image, env) -> Deployment:
         api = client.AppsV1Api()
         spec = client.V1DeploymentSpec(
             replicas=1,
-            selector=client.V1LabelSelector(
-                match_labels={"app": name},
-            ),
             template=client.V1PodTemplateSpec(
-                metadata=client.V1ObjectMeta(
-                    labels={"app": name},
-                ),
                 spec=client.V1PodSpec(
                     service_account_name=service_account.name,
                     containers=[
                         client.V1Container(
                             name=name,
-                            image="quay.io/mongodb/mongodb-enterprise-operator:1.2.3",
+                            image=container_image,
                             image_pull_policy="IfNotPresent",
-                            env=[
-                                client.V1EnvVar(
-                                    name="OPERATOR_ENV",
-                                    value="dev",
-                                ),
-                                client.V1EnvVar(
-                                    name="WATCH_NAMESPACE",
-                                    value=namespace,
-                                ),
-                                client.V1EnvVar(
-                                    name="CURRENT_NAMESPACE",
-                                    value=namespace,
-                                ),
-                                client.V1EnvVar(
-                                    name="MANAGED_SECURITY_CONTEXT",
-                                    value="true",
-                                ),
-                                client.V1EnvVar(
-                                    name="MONGODB_ENTERPRISE_DATABASE_IMAGE",
-                                    value="quay.io/mongodb/mongodb-enterprise-database:1.2.3"
-                                ),
-                                client.V1EnvVar(
-                                    name="IMAGE_PULL_POLICY",
-                                    value="Always"
-                                ),
-                                client.V1EnvVar(
-                                    name="OPS_MANAGER_IMAGE_REPOSITORY",
-                                    value="",
-                                ),
-                                client.V1EnvVar(
-                                    name="OPS_MANAGER_IMAGE_PULL_POLICY",
-                                    value="",
-                                ),
-                            ],
+                            env=env,
                         ),
                     ],
                 ),
