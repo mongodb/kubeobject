fb4562fe99bdfe7fb23ff3f1e09b50c454f9d747
Rodrigo Valin <licorna@gmail.com>
2019-09-19 19:15:48 +0100

    Added docs for pypi.

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="kubeobject",
-    version="0.1.1",
+    version="0.1.2",
 
     author="Rodrigo Valin",
     author_email="licorna@gmail.com",
     description="Easily Manage Kubernetes Objects.",
+    url='https://gitlab.com/licorna/kubeobject',
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
     packages=find_packages(),
 )
