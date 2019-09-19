07840f5cc196855d58dfb1229e6a7bec2aff77ed
Rodrigo Valin <licorna@gmail.com>
2019-09-19 14:12:29 +0100

    Bumping version again.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kubeobject",
-    version="0.1.0",
+    version="0.1.1",
 
     author="Rodrigo Valin",
     author_email="licorna@gmail.com",
