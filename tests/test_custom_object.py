a35c6d1dd4c4851a3158e82dc373f44c7c393003
Rodrigo Valin <rodrigo.valin@mongodb.com>
2019-09-24 22:40:57 +0100

    Added tests to auto_reload functionality.

--- tests/test_custom_object.py
+++ tests/test_custom_object.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+
+from freezegun import freeze_time
 from unittest import mock
 from types import SimpleNamespace
 
@@ -5,12 +8,18 @@ from kubeobject.kubeobject import CustomObject
 
 
 def mocked_custom_api():
+    stored_body = []
+
     class MockedApi:
         def get_namespaced_custom_object(group, version, namespace, plural, name):
+            if len(stored_body) > 0:
+                return stored_body[-1]
             return {"name": name}
 
-        def create_namespaced_custom_object(group, version, namespace, plural, body):
-            return {"name": body["metadata"]["name"]}
+        def create_namespaced_custom_object(group, version, namespace, plural, body: dict):
+            body.update({"name": body["metadata"]["name"]})
+            stored_body.append(body)
+            return body
 
     return MockedApi
 
@@ -85,3 +94,41 @@ metadata:
         m.assert_called_once_with("some-other-file.yaml")
 
         assert a.__class__.__name__ == "Subklass"
+
+
+@mock.patch("kubeobject.kubeobject.client.CustomObjectsApi", return_value=mocked_custom_api())
+@mock.patch("kubeobject.kubeobject.get_crd_names", return_value=mocked_crd_return_value())
+def test_custom_object_defined(mocked_crd_return_value, mocked_client):
+    klass = CustomObject.define("Dummy", plural="dummies", api_version="dummy.com/v1")
+
+    k = klass("my-dummy", "default").create()
+
+    assert k.__class__.__name__ == "_defined"
+
+
+@mock.patch("kubeobject.kubeobject.client.CustomObjectsApi", return_value=mocked_custom_api())
+@mock.patch("kubeobject.kubeobject.get_crd_names", return_value=mocked_crd_return_value())
+def test_custom_object_auto_reload(mocked_get_crd_names, mocked_client):
+    klass = CustomObject.define("Dummy", plural="dummies", api_version="dummy.com/v1")
+
+    k = klass("my-dummy", "default")
+    assert k.last_update is None
+
+    k["status"] = "something"
+    k.create()
+    assert k.last_update is not None
+
+    k.auto_reload = True
+    assert k["status"] == "something"
+    assert k.last_update is not None
+    last_update_recorded = k.last_update
+
+    # If getting the status after 1 second, we don't reload
+    with freeze_time(datetime.now() + timedelta(milliseconds=1000)):
+        k["status"]
+        assert k.last_update == last_update_recorded
+
+    # If getting the status after 2.1 seconds, we reload!
+    with freeze_time(datetime.now() + timedelta(milliseconds=2100)):
+        k["status"]
+        assert k.last_update > last_update_recorded
