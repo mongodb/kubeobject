7c188238b6a40cd824c24776c68c464ed8c633eb
Rodrigo Valin <licorna@gmail.com>
2019-09-25 12:45:30 +0100

    Gets version from tag.

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+import os
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+commit_tag = os.getenv("CI_COMMIT_TAG", "latest")
+stage = os.getenv("CI_JOB_STAGE", "local")
+
+if stage == "publish" and commit_tag == "latest":
+    raise ValueError("Did not get a commit tag during publishing.")
+
+if stage == "publish":
+    commit_tag = commit_tag.split("-")[1]
+
 setup(
     name="kubeobject",
-    version="0.1.4",
+    version=commit_tag,
 
     author="Rodrigo Valin",
     author_email="licorna@gmail.com",
