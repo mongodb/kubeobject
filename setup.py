7f9607f77acaa54c4280aadae5f0c6e05ec9337e
Rodrigo Valin <rodrigo.valin@mongodb.com>
2021-08-25 10:53:19 +0200

    Release action. (#2)

--- setup.py
+++ setup.py
@@ -1,26 +1,7 @@
-import os
-import re
-
 from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
-
-def get_version():
-    commit_tag = os.getenv("CI_COMMIT_TAG", "latest")
-    stage = os.getenv("CI_JOB_STAGE", "local")
-
-    if stage == "publish":
-        if commit_tag == "latest":
-            raise ValueError("Did not get a commit tag during publishing.")
-
-        if not re.match(r"^release-\d+\.\d+\.\d+$", commit_tag):
-            raise ValueError("Wrong tag format.")
-
-        commit_tag = commit_tag.split("-")[1]
-
-    return commit_tag
 
 
 packages = []
@@ -29,12 +10,12 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="kubeobject",
-    version=get_version(),
+    version="0.2.0",
 
     author="Rodrigo Valin",
     author_email="rodrigo.valin@mongodb.com",
     description="Easily Manage Kubernetes Objects.",
-    url='https://github.com/10gen/kubeobject',
+    url='https://github.com/mongodb/kubeobject',
 
     long_description=long_description,
     long_description_content_type="text/markdown",
